52dc16503d0b89502cd0930dba82cc8e0ea885f4
auvararaksin <auvaraksin@mail.ru>
2025-04-20 21:57:10 +0200

    TestServiceImplTest.java was updated and ApplicationTest.java was deleted after code review

--- hw01-xml-config/src/test/java/ru/otus/hw/ApplicationTest.java
+++
@@ -1,4 +0,0 @@
-package ru.otus.hw;
-
-public class ApplicationTest {
-}
--- hw01-xml-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw01-xml-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -3,31 +3,38 @@ package ru.otus.hw.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import ru.otus.hw.dao.CsvQuestionDao;
 
 
-import static org.mockito.Mockito.*;
 
 public class TestServiceImplTest {
 
+    @Mock
     private IOService ioService;
+
+    @Mock
     private QuestionOutputViewService questionOutputViewService;
+
+    @Mock
     private CsvQuestionDao questionDao;
+
+    @InjectMocks
     private TestServiceImpl testService;
 
 
     @BeforeEach
     void setUp() {
-        ioService = mock(StreamsIOService.class);
-        questionOutputViewService = mock(QuestionConsoleOutput.class);
-        questionDao = mock(CsvQuestionDao.class);
-        testService = new TestServiceImpl(ioService, questionOutputViewService, questionDao);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     @DisplayName("Should invoke one time execute test method")
     void shouldInvokeOneTimeExecuteTestMethod() {
         testService.executeTest();
-        verify(questionDao, times(1)).findAll();
+        Mockito.verify(questionDao, Mockito.times(1)).findAll();
     }
 }
